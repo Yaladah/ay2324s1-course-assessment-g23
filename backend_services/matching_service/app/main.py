61379f1227b8f9dea690545a41142a1a9cdfb4a3
Yaladah <jeromeseet@hotmail.com>
2023-09-30 16:16:05 +0800

    Implement matching logic in matching service

--- backend_services/matching_service/app/main.py
+++ backend_services/matching_service/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import threading
 
 from matching_util import User
-from queue_manager import queue, check_for_matches
+from queue_manager import consume_queue, check_for_matches, send_user_to_queue
 
 # create app
 app = FastAPI()
@@ -31,9 +31,11 @@ async def websocket_endpoint(websocket: WebSocket):
         request =  json.loads(message)
         detail = request["detail"]
         user_id = detail["user_id"]
+        complexity = detail["complexity"]
 
-        user = User(user_id, websocket)
-        queue.append(user)
+        send_user_to_queue(user_id, complexity)
+
+        consume_queue(f'{complexity}_queue')
 
     except HTTPException as http_exc:
         await websocket.send_text(http_exc.detail)
