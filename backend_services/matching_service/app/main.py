21350b2c8a0b218eec25e105b7f682fe1441b431
Yaladah <jeromeseet@hotmail.com>
2023-10-06 14:12:57 +0800

    Change websocket endpoints - Not working

--- backend_services/matching_service/app/main.py
+++ backend_services/matching_service/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException,  WebSocket
+from typing import Optional
+from fastapi import FastAPI, HTTPException, Query,  WebSocket
 import json
 from fastapi.middleware.cors import CORSMiddleware
 import threading
-
+import websockets
 from matching_util import User
 from matching import send_user_to_queue, listen_for_server_replies
 # from queue_manager import consume_queue, check_for_matches, send_user_to_queue
@@ -12,21 +13,21 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 app = FastAPI()
 
-@app.websocket("/ws/matching")
+@app.websocket("/matching/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         # Receive message from client
         message = await websocket.receive_text()
-        print("Message: ", message)
         request =  json.loads(message)
         detail = request["message"]
         user_id = detail["user_id"]
@@ -41,6 +42,12 @@ async def websocket_endpoint(websocket: WebSocket):
         listener_thread.start()
         await websocket.close()
 
+    except websockets.exceptions.ConnectionClosedError as conn_closed_exc:
+        # Handle WebSocket connection closed errors
+        print(f"WebSocket connection closed: {conn_closed_exc}")
     except HTTPException as http_exc:
         await websocket.send_text(http_exc.detail)
+    except Exception as e:
+        # Log any other exceptions for debugging
+        print(f"An error occurred: {e}")
 
